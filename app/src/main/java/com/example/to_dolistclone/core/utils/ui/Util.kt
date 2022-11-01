bd6f4132f43c2ad11fa2722df75e3922d5076d73
Joevincent <jvincent1703@gmail.com>
2022-11-01 13:25:23 +0700

    added pulse animation for fab

---
+++ app/src/main/java/com/example/to_dolistclone/core/utils/ui/Util.kt
@@ -0,0 +1,52 @@
+package com.example.to_dolistclone.core.utils.ui
+
+import android.app.Activity
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+fun Activity.makeToast(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.makeToast(msg: String) {
+    Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+}
+
+fun <T> AppCompatActivity.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collect)
+        }
+    }
+}
+
+fun <T> Fragment.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    viewLifecycleOwner.lifecycleScope.launch {
+        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collect)
+        }
+    }
+}
+
+fun <T> AppCompatActivity.collectLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collect(collect)
+        }
+    }
+}
+
+fun <T> Fragment.collectLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    viewLifecycleOwner.lifecycleScope.launch {
+        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collect(collect)
+        }
+    }
+}
--- app/src/main/java/com/example/to_dolistclone/feature/tasks/ui/TasksFragment.kt
+++ app/src/main/java/com/example/to_dolistclone/feature/tasks/ui/TasksFragment.kt
@@ -1,7 +1,11 @@
 package com.example.to_dolistclone.feature.tasks.ui
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import com.example.to_dolistclone.core.utils.ui.makeToast
 import com.example.to_dolistclone.databinding.FragmentTasksBinding
 import com.example.to_dolistclone.feature.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +15,31 @@ class TasksFragment : BaseFragment<FragmentTasksBinding>(FragmentTasksBinding::i
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.fabFade.pulseAnimation()
+
+        binding.add.setOnClickListener {
+            makeToast("navigate to details fragment")
+        }
+    }
+
+    private fun ImageView.pulseAnimation(
+        scaleX: Float = 1.5F,
+        ScaleY: Float = 1.5F,
+        alpha: Float = 0F,
+        duration: Long = 1000,
+        repeatCount: Int = ObjectAnimator.INFINITE,
+        repeatMode: Int = ObjectAnimator.RESTART
+    ) {
+        val pulse = ObjectAnimator.ofPropertyValuesHolder(
+            binding.fabFade,
+            PropertyValuesHolder.ofFloat("scaleX", scaleX),
+            PropertyValuesHolder.ofFloat("scaleY", ScaleY),
+            PropertyValuesHolder.ofFloat("alpha", alpha)
+        )
+        pulse.duration = duration
+        pulse.repeatCount = repeatCount
+        pulse.repeatMode = repeatMode
+        pulse.start()
     }
 
 }
